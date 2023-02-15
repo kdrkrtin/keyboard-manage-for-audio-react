681f4d6b0c18ce7a7aa0d1e550372dad8cd6d86e
Kadir Kartin <62345273+kdrkrtin@users.noreply.github.com>
2023-02-15 11:41:56 +0300

    Dark mode was edited.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import axios from "axios";
 
-export const reqAxios = axios.create({
-    baseURL: 'https://hm-sahne---ses---react-default-rtdb.firebaseio.com/'
-})
+const mode = localStorage.getItem("mode");
+if (mode === "dark") {
+  document.body.classList.add("dark");
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
